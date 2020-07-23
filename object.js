26487d845820f5cb9745dfeb7f47927b5a0c6abb
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-07-23 19:19:04 +0100

    more objects

---
+++ object.js
@@ -0,0 +1,44 @@
+
+/*let car = {
+    make: 'BMW',
+    model: '745li',
+    year: 2010,
+    getPrice: function () {
+        //perform some calc
+        return 5000;
+        
+    },
+    printDescription: function () {
+        console.log(this.make + ' ' + this.model);
+        
+    }
+}
+*/
+/*
+car.printDescription();
+console.log(car.year);
+*/
+//console.log(car['year']);
+//console.log(car[1]);
+
+/*var anotherCar = {};
+anotherCar.whatever = 'bob';
+console.log(anotherCar.whatever);*/
+
+/*
+var c = {
+    myPropety: [
+     {d: 'this' },
+     {e: 'can'},
+     {f: 'get'},
+     {g: 'crazy'}
+    ]
+};
+*/
+
+let carLot = [
+    {year: 2017, make: 'Toyota', model: '4Runner'},
+    {year: 2015, make: 'BMW', model: '5281'},
+    {year: 1892, make: 'Buick', model: 'Skylark'},
+];
+

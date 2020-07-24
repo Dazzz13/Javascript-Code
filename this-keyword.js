1aa805a7ab240cbe5a7a3f308858c7419f824fca
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-07-24 15:25:06 +0100

    this-keyword

--- this-keyword.js
+++ this-keyword.js
@@ -29,16 +29,40 @@ function second() {
 console.log(second() === global);
 console.log(second() === undefined); */
 
-let myObject = {value: 'My Object' };
+/* let myObject = {value: 'My Object' };
 
+//This property is set on the global object//
 global.value = 'Global object';
 
 function third() {
+    //Returns something different depending on how we call this method//
     return this.value;
     
 }
 
 //console.log(third());
 
+//Both call and apply allow you to explicitly set what you want to represent 'this'. //
+//The difference is in how additional arguments by the function are sent//
+
 console.log(third.call(myObject, 'bob'));
-console.log(third.apply(myObject, ['bob']));
+console.log(third.apply(myObject, ['bob'])); */
+
+function fifth() {
+    console.log(this.firstName + ' ' + this.lastName);
+    
+}
+
+let customer1 = {
+    firstName: 'bob',
+    lastName: 'tabor',
+    print: fifth
+}
+let customer2 = {
+    firstName: 'richard',
+    lastName: 'boughton',
+    print: fifth
+}
+
+customer1.print();
+customer2.print();

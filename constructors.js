9af6ef2cd88a1d48c97111cb386e2185c8260f47
Darragh McEvoy <48887407+Dazzz13@users.noreply.github.com>
2020-08-04 12:43:34 +0100

    constructors, natives and prototypes

---
+++ constructors.js
@@ -0,0 +1,11 @@
+// class Car {
+//     constructor(make, model, year) {
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//     }
+// }
+
+// let MyCar = new Car('bmw', '745li', 2010);
+
+// console.log(MyCar);
